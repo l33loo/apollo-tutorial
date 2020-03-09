9e7323552cc9cc30893a1ea07d75e376803e74d7
Lila Karpowicz <lila@karpowicz.dev>
2020-03-09 14:04:53 -0700

    Add resolver for mission type

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -22,5 +22,12 @@ module.exports = {
         launch: (_, { id }, { dataSources }) =>
             dataSources.launchAPI.getLaunchById({ launchId: id }),
         me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
-    }
+    },
+    Mission: {
+        missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+            return size === 'SMALL'
+                ? mission.missionPatchSmall
+                : mission.missionPatchLarge;
+        },
+    },
 };
